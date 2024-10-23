84797b34513b9fb6349bcee5d10e4c40af22fc4a
saki0411 <chipdale411@gmail.com>
2024-10-23 20:24:06 +0900

    fix

--- AnimalShogi/SyougiView.swift
+++ AnimalShogi/SyougiView.swift
@@ -10,169 +10,81 @@ import SwiftUI
 struct SyougiView: View {
     let boardRows = 4
     let boardCols = 3
-     @StateObject var game = Game()
+    @StateObject var game = Game()
     var body: some View {
         ZStack{
-         BackGroundView()
-                .padding(.bottom, 25)
-            VStack{
-                ButtonView()
             
+            VStack{
+                Text("\(game.currentPlayer == .player1 ? "先手" : "後手")の番です")
+                              .font(.headline)
+                              .padding()
                 
                 VStack(spacing: 1) {
                     ForEach(0..<4) { row in
                         HStack(spacing: 1) {
                             ForEach(0..<3) { col in
-                                    
-                                    CellView(piece: game.gameBoard.board[row][col])
-                                        .onTapGesture {
-                                            let position = Position(row: row, col: col)
-                                            // 駒の移動処理
-                                            if let selected = game.selectedPiecePosition {
-                                                print("move", selected, position)
-                                                _ = game.movePiece(to: position)
-                                            } else {
-                                                print("select", position)
-                                                game.selectPiece(at: position)
-                                            }
-                                            //                                game.movePiece(to: position)
-                                        
-                                }
+                                
+                                CellView(piece: game.gameBoard.board[row][col],
+                                         isSelected: game.selectedPiecePosition == Position(row: row, col: col))
+                                    .onTapGesture {
+                                        let position = Position(row: row, col: col)
+                                        // 駒の移動処理
+                                        if let selected = game.selectedPiecePosition {
+                                            print("move", selected, position)
+                                            _ = game.movePiece(to: position)
+                                        } else {
+                                            print("select", position)
+                                            game.selectPiece(at: position)
+                                        }
+                                       
+                                    }
+                                
                             }
                         }
                     }
                 }
                 
-                ButtonView()
-                    .padding(.top,30)
-            }
-           
                 
             }
+            .padding(.horizontal,100)
+            
+            
         }
     }
+}
 
 struct ButtonItemView: View {
     var body: some View {
         Circle()
             .fill(Color.gray)
             .frame(width: 42)
-           
+        
     }
 }
 
-struct ButtonView: View{
-    var body: some View {
-        ZStack{
-            //左下
-            ZStack{
-                ButtonItemView()
-                Text("")
-                    .foregroundStyle(.white)
-            }
-            .onTapGesture {
-                
-            }
-            .padding(.top,  100)
-            .padding(.leading, 100)
-            
-            //右上
-            ZStack{
-                ButtonItemView()
-                Text("")
-                    .foregroundStyle(.white)
-            }
-            .padding(.bottom,  100)
-            .padding(.leading, 100)
-            .onTapGesture {
-                
-            }
-            //左下
-            ZStack{
-                ButtonItemView()
-                Text("")
-                    .foregroundStyle(.white)
-            }
-            .padding(.top,  100)
-            .padding(.trailing, 100)
-            .onTapGesture {
-                
-            }
-            
-            //左上
-            ZStack{
-                ButtonItemView()
-                Text("")
-                    .foregroundStyle(.white)
-            }
-            .padding(.bottom,  100)
-            .padding(.trailing, 100)
-            .onTapGesture {
-                
-            }
-            
-            VStack{
-                ZStack{
-                    ButtonItemView()
-                    Text("↑")
-                        .foregroundStyle(.white)
-                }
-                
-                .onTapGesture {
-                    
-                }
-                
-                HStack{
-                    
-                    ZStack{
-                        ButtonItemView()
-                        Text("←")
-                            .foregroundStyle(.white)
-                    }
-                    .padding(.trailing, 50)
-                    .onTapGesture {
-                        
-                    }
-                    ZStack{
-                        ButtonItemView()
-                        Text("→")
-                            .foregroundStyle(.white)
-                    }
-                    .onTapGesture {
-                        
-                    }
-                    
-                }
-                ZStack{
-                    ButtonItemView()
-                    Text("↓")
-                        .foregroundStyle(.white)
-                }
-                
-            }
-        }
-    }
-  
-    
-   
-}
 #Preview {
     SyougiView()
 }
 
 struct CellView: View {
-  
+    
     let piece: Piece?
+    let isSelected: Bool
     var body: some View {
-      
+        ZStack{
+            
+            Rectangle()
+                .fill(isSelected ? Color.blue.opacity(0.3) : Color.yellow.opacity(0.3))
+                .frame(width: 100, height: 100)
             
             if let piece = piece {
                 Image(pieceDisplay(piece: piece))
                     .resizable()
                     .scaledToFit()
                     .padding(.all, 10)
+                    .rotationEffect(piece.player == .player1 ? .degrees(180) : .degrees(0))
             }
-        
+        }
     }
     func pieceDisplay(piece: Piece) -> String {
         switch piece.type {
