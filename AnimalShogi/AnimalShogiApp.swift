76b4c0534713875190b6b027cb11e652b4e48ae8
saki0411 <chipdale411@gmail.com>
2024-10-23 19:07:41 +0900

    add-yazirusi

--- AnimalShogi/AnimalShogiApp.swift
+++ AnimalShogi/AnimalShogiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AnimalShogiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SyougiView()
         }
     }
 }
--- AnimalShogi/SyougiView.swift
+++ AnimalShogi/SyougiView.swift
@@ -8,21 +8,176 @@
 import SwiftUI
 
 struct SyougiView: View {
+    let boardRows = 4
+    let boardCols = 3
     var body: some View {
         ZStack{
-            BackGroundView()
+            //    BackGroundView()
             VStack{
-                Circle()
-                    .fill(Color.red)
-                    .frame(width: 40)
+                
+                ButtonView()
+                
+                
+                VStack(spacing: 1) {
+                    ForEach(0..<4) { row in
+                        HStack(spacing: 1) {
+                            ForEach(0..<3) { col in
+                                CellView()
+                            }
+                        }
+                    }
+                }
+                
+                ButtonView()
+                    .padding(.top,30)
             }
-            .padding(.top, 630)
-         
+               
+                
+            }
+        }
+    }
 
+struct ButtonItemView: View {
+    var body: some View {
+        Circle()
+            .fill(Color.gray)
+            .frame(width: 42)
+           
+    }
+}
+
+struct ButtonView: View{
+    var body: some View {
+        ZStack{
+            //左下
+            ZStack{
+                ButtonItemView()
+                Text("")
+                    .foregroundStyle(.white)
+            }
+            .onTapGesture {
+                
+            }
+            .padding(.top,  100)
+            .padding(.leading, 100)
+            
+            //右上
+            ZStack{
+                ButtonItemView()
+                Text("")
+                    .foregroundStyle(.white)
+            }
+            .padding(.bottom,  100)
+            .padding(.leading, 100)
+            .onTapGesture {
+                
+            }
+            //左下
+            ZStack{
+                ButtonItemView()
+                Text("")
+                    .foregroundStyle(.white)
+            }
+            .padding(.top,  100)
+            .padding(.trailing, 100)
+            .onTapGesture {
+                
+            }
+            
+            //左上
+            ZStack{
+                ButtonItemView()
+                Text("")
+                    .foregroundStyle(.white)
+            }
+            .padding(.bottom,  100)
+            .padding(.trailing, 100)
+            .onTapGesture {
+                
+            }
+            
+            VStack{
+                ZStack{
+                    ButtonItemView()
+                    Text("↑")
+                        .foregroundStyle(.white)
+                }
+                .onTapGesture {
+                    
+                }
+                
+                HStack{
+                    
+                    ZStack{
+                        ButtonItemView()
+                        Text("←")
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing, 50)
+                    .onTapGesture {
+                        
+                    }
+                    ZStack{
+                        ButtonItemView()
+                        Text("→")
+                            .foregroundStyle(.white)
+                    }
+                    .onTapGesture {
+                        
+                    }
+                    
+                }
+                ZStack{
+                    ButtonItemView()
+                    Text("↓")
+                        .foregroundStyle(.white)
+                }
+                
+            }
+        }
+    }
+  
+    
+   
+}
+#Preview {
+    SyougiView()
+}
+
+struct CellView: View {
+    //   let piece: AnimalShogiGame.BoardPiece
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(Color.yellow.opacity(0.3))
+                .frame(width: 100, height: 100)
+            
+            //            if piece.piece != .none {
+            //                Text(piece.piece.image)
+            //                    .font(.system(size: 40))
+            //                    .rotationEffect(piece.player == .second ? .degrees(180) : .degrees(0))
+            //            }
         }
     }
 }
 
-#Preview {
-    SyougiView()
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
