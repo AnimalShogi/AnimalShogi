0308338f4116c4b93f196d582a6a9c4ea9a255cc
tiger121005 <itiger4649@gmail.com>
2024-10-23 18:02:16 +0900

    first commit

---
+++ AnimalShogi/AnimalShogiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalShogiApp.swift
+//  AnimalShogi
+//
+//  Created by TAIGA ITO on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalShogiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
